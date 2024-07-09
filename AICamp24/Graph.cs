4b9ddd1c09cc8674144a1f97d2000253443053e7
Mason Lee <asonmay09@gmail.com>
2024-07-08 21:01:34 -0700

    added a graph generation function that isnt working

--- AICamp24/Graph.cs
+++ AICamp24/Graph.cs
@@ -16,54 +16,6 @@ namespace AICamp2024
 
         public void AddEdge(Node<T> startingNode, Node<T> endingNode) => startingNode.AddNeighbor(endingNode);
 
-        public List<Node<T>> DepthFirstSearch(Node<T> startingNode, Node<T> endingNode)
-        {
-            List<Node<T>> nodes = new List<Node<T>>();
-            Node<T> currentNode = startingNode;
-            Stack<Node<T>> frontier = new Stack<Node<T>>();
-
-            while(currentNode != endingNode)
-            {
-                for(int i = 0; i < currentNode.Neighbors.Count; i++)
-                {
-                    Node<T> neighbor = currentNode.Neighbors[i].EndingNode;
-                    if (!nodes.Contains(neighbor))
-                    {
-                        frontier.Push(neighbor);
-                    }
-                }
-                nodes.Add(currentNode);
-                currentNode = frontier.Pop();
-            }
-
-            nodes.Add(currentNode);
-            return nodes;
-        }
-
-        public List<Node<T>> BreathFirstSearch(Node<T> startingNode, Node<T> endingNode)
-        {
-            List<Node<T>> nodes = new List<Node<T>>();
-            Node<T> currentNode = startingNode;
-            Queue<Node<T>> frontier = new Queue<Node<T>>(); 
-
-            while (currentNode != endingNode)
-            {
-                for (int i = 0; i < currentNode.Neighbors.Count; i++)
-                {
-                    Node<T> neighbor = currentNode.Neighbors[i].EndingNode;
-                    if (!frontier.Contains(neighbor) && !nodes.Contains(neighbor))
-                    {
-                        frontier.Enqueue(neighbor);
-                    }
-                }
-                nodes.Add(currentNode);
-                currentNode = frontier.Dequeue();
-            }
-
-            nodes.Add(currentNode);
-            return nodes;
-        }
-
         public List<Node<T>> Search(Node<T> startingNode, Node<T> endingNode, Func<List<Node<T>>, Node<T>> selection)
         {
             List<Node<T>> nodes = new List<Node<T>>();
--- AICamp24/Program.cs
+++ AICamp24/Program.cs
@@ -19,13 +19,13 @@ namespace AICamp2024
             {
                 for (int y = 0; y < size.Y; y++)
                 {
-                    if (x + 1 != size.X)
+                    if (x + 1 < size.X)
                     {
-                        graph.Nodes[x * size.Y + y].AddNeighbor(graph.Nodes[(x + 1) * size.Y + y]);
+                        graph.Nodes[y * size.X + x].AddNeighbor(graph.Nodes[y * size.X + x + 1]);
                     }
-                    if (y + 1 != size.Y)
+                    if (y + 1 < size.Y)
                     {
-                        graph.Nodes[x * size.Y + y].AddNeighbor(graph.Nodes[x * size.Y + y + 1]);
+                        graph.Nodes[y * size.X + x].AddNeighbor(graph.Nodes[(y + 1) * size.X + x]);
                     }
                 }
             }
@@ -51,10 +51,7 @@ namespace AICamp2024
         {
             Graph<int> graph = generateGraph(new Point(4, 3));
 
-            Func<List<Node<int>>, Node<int>> selection = BFSSelection;
-
-            List<Node<int>> depthFirstSearch = graph.DepthFirstSearch(graph.Nodes[0], graph.Nodes[8]);
-            List<Node<int>> breathFirstSearch = graph.BreathFirstSearch(graph.Nodes[0], graph.Nodes[8]);
+            Func<List<Node<int>>, Node<int>> selection = DFSSelection;
 
             List<Node<int>> path = graph.Search(graph.Nodes[0], graph.Nodes[8], selection);
             ;
