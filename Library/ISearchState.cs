65e908cd40430c77264045d56dda8b9f541b637e
Mason Lee <asonmay09@gmail.com>
2024-07-11 20:44:17 -0700

    Added Library

---
+++ Library/ISearchState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public interface ISearchState<T>
+    {
+        public List<ISearchState<T>> GetSuccessors(T node, List<T> successors);
+
+        public bool Equals(ISearchState<T> other);
+    }
+}
