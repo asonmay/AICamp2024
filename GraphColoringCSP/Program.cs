7692b6ba37e2f1f7339d3bacc17ca8a326209b8d
Mason Lee <asonmay09@gmail.com>
2024-07-17 18:17:37 -0700

    fixed graph coloring CSP

--- GraphColoringCSP/Program.cs
+++ GraphColoringCSP/Program.cs
@@ -32,25 +32,35 @@ namespace GraphColoringCSP
             Constrant<Color>[] constrants = new Constrant<Color>[]
             {
                 new Constrant<Color>(isEqual, new List<Point>{new Point(0,0), new Point(1,0)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(0,0), new Point(0,1)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(1,0), new Point(2,0)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(1,0), new Point(1,1)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(1,0), new Point(0,0)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(1,0), new Point(0,1)}),
                 new Constrant<Color>(isEqual, new List<Point>{new Point(2,0), new Point(1,0)}),
-                new Constrant<Color>(isEqual, new List<Point>{new Point(0,1), new Point(1,0)}),
-                new Constrant<Color>(isEqual, new List<Point>{new Point(1,1), new Point(1,0)}),
                 new Constrant<Color>(isEqual, new List<Point>{new Point(2,0), new Point(2,1)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(2,0), new Point(1,1)}),
                 new Constrant<Color>(isEqual, new List<Point>{new Point(0,1), new Point(1,1)}),
-                new Constrant<Color>(isEqual, new List<Point>{new Point(0,1), new Point(0,2)}),
                 new Constrant<Color>(isEqual, new List<Point>{new Point(0,1), new Point(0,0)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(0,1), new Point(1,0)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(0,1), new Point(0,2)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(1,1), new Point(1,0)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(1,1), new Point(0,2)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(1,1), new Point(0,1)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(1,1), new Point(1,2)}),
                 new Constrant<Color>(isEqual, new List<Point>{new Point(1,1), new Point(2,1)}),
                 new Constrant<Color>(isEqual, new List<Point>{new Point(1,1), new Point(2,0)}),
-                new Constrant<Color>(isEqual, new List<Point>{new Point(1,1), new Point(0,2)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(1,2), new Point(1,1)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(1,2), new Point(2,1)}),
                 new Constrant<Color>(isEqual, new List<Point>{new Point(1,2), new Point(2,2)}),
-                new Constrant<Color>(isEqual, new List<Point>{new Point(2,1), new Point(2,2)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(0,2), new Point(1,2)}),
+                new Constrant<Color>(isEqual, new List<Point>{new Point(2,2), new Point(1,2)}),
             };
 
             Graph<Color> graph = new Graph<Color>();
             graph.AddNode(variables);
 
             Variable<Color>[,] solution = graph.GetSolution(new NodeWrapper<Color>(graph.Nodes[0],constrants.ToList(),new Point(0,0)), DFSSelection);
-            ;
         }
     }
 }
